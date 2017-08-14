833083e0448b2914c6c7f59ea38bb70aaffe1f24
nealbaker1958 <30833107+nealbaker1958@users.noreply.github.com>
2017-08-14 23:57:58 +0100

    Update delete-accenture-stack.sh

--- delete-accenture-stack.sh
+++ delete-accenture-stack.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-/usr/local/aws/bin/aws cloudformation delete-stack --stack-name AccentureDE1Stack --region eu-central-1
+ENV=${1}
 
-/usr/local/aws/bin/aws cloudformation delete-stack --stack-name AccentureSG1Stack --region ap-southeast-1
+cp /home/ubuntu/.aws/credentials.$ENV /home/ubuntu/.aws/credentials
+
+#/usr/local/aws/bin/aws cloudformation delete-stack --stack-name AccentureDE1Stack --region eu-central-1
+
+#/usr/local/aws/bin/aws cloudformation delete-stack --stack-name AccentureSG1Stack --region ap-southeast-1
 
 /usr/local/aws/bin/aws cloudformation delete-stack --stack-name AccentureWA1Stack --region us-east-1
 
